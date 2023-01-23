e76ab5d441bbe5fd2374e223be2a41fe548e8d1c
juanpe <jplazcano93@gmail.com>
2023-01-23 20:58:23 +0000

    configuracion set page streamlit

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from PIL import Image
 import plotly.express as px
 import plotly.graph_objects as go
 
+st.set_page_config(page_title='JP Analytics', page_icon=None, layout="centered", initial_sidebar_state="auto", menu_items=None)
+
 image = Image.open('/Users/juanpe/Desktop/dashboard_moguillansky/Screenshot 2023-01-23 at 20.14.19.png')
 st.image(image)
 
@@ -31,6 +33,10 @@ if df is not None:
     df_sin_insumos = df[df['Especialidad'].isin(['Ecografia', 'Resonancia', 'Radiologia', 'Angio RM',
                                                  'Mamografia', 'Doppler', 'Tomografia', 'Densitometria',
                                                  'Puncion por Eco', 'Angio TAC', 'Puncion por TAC'])]
+
+    # La clínica tiene que facturar material de contraste y descartable a veces en la especialidad incorrecta, no es
+    # un error, sino que así lo deben hacer. Para el análisis hay que arreglarlo para obtener la cantidad correcta de
+    # estudios:
 
     df.loc[(df["Practica"] == "Material de contraste para tomografía") & (
                 df["Especialidad"] == "Tomografia"), "Especialidad"] = "Contrastes"
