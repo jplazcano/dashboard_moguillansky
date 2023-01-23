ccdb8aa15f07c82a67a29f6a47fbceb11b9fad31
juanpe <jplazcano93@gmail.com>
2023-01-23 20:16:34 +0000

    logo y carga de archivo excel

--- main.py
+++ main.py
@@ -3,3 +3,31 @@ import pandas as pd
 from PIL import Image
 import plotly.express as px
 import plotly.graph_objects as go
+
+image = Image.open('/Users/juanpe/Desktop/dashboard_moguillansky/Screenshot 2023-01-23 at 20.14.19.png')
+st.image(image)
+
+# TITULO / INSTRUCCIONES
+archivo = st.sidebar.file_uploader("", type="xls", accept_multiple_files=True)
+if archivo is not None:
+    dfs = []
+    if len(archivo) > 1:
+        for f in archivo:
+            df = pd.read_excel(f)
+            dfs.append(df)
+        df = pd.concat(dfs)
+    elif len(archivo) == 1:
+        df = pd.read_excel(archivo[0])
+    else:
+        df = None
+
+    # SECCIÓN 1: Análisis General
+if df is not None:
+
+    # Agrego número de día, mes y año
+    df['Año'] = df['Fecha Turno'].dt.year
+    df['Mes'] = df['Fecha Turno'].dt.month
+    df['Día'] = df['Fecha Turno'].dt.weekday
+    df_sin_insumos = df[df['Especialidad'].isin(['Ecografia', 'Resonancia', 'Radiologia', 'Angio RM',
+                                                 'Mamografia', 'Doppler', 'Tomografia', 'Densitometria',
+                                                 'Puncion por Eco', 'Angio TAC', 'Puncion por TAC'])]
